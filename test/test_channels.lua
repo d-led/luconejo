d27d2409ab342b33349f8d19ff0a59c74651dd52
Dmitry Ledentsov <dmlled@yahoo.com>
2013-12-07 23:20:24 +0100

    channel reuse

--- test/test_channels.lua
+++ test/test_channels.lua
@@ -12,8 +12,16 @@ local connected_test = {
 
 describe("first channel", function()
 	local this = connected_test.create()
+	
 	it("should be able to create and delete exchanges", function ()
 		assert.True( this.channel:DeclareExchange("test_channel_exchange", luconejo.Channel.EXCHANGE_TYPE_FANOUT, false, false, true) )
 		assert.True( this.channel:DeleteExchange("test_channel_exchange") )
+	end)
+
+	it("should be possible to reuse channels",function()
+		assert.True( this.channel:DeclareExchange("test_channel_exchange1", luconejo.Channel.EXCHANGE_TYPE_FANOUT, false, false, true) )
+		assert.True( this.channel:DeclareExchange("test_channel_exchange2", luconejo.Channel.EXCHANGE_TYPE_FANOUT, false, false, true) )
+		assert.True( this.channel:DeleteExchange("test_channel_exchange1") )
+		assert.True( this.channel:DeleteExchange("test_channel_exchange2") )
 	end)
 end)
